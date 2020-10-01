8e18c62f26fd97d382d08a534151cd2c356efa35
Ricardo Navarro <ricardonavarro2611@gmail.com>
2020-10-01 18:27:01 -0300

    Criando o projeto de xadrez no terminal

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Hello World!";
+            Console.WriteLine(texto);
+        }
+    }
+}
